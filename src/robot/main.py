50063fc266fd255e816c8bed6e462484010d1cbb
zuiken-code <zuiken2022robo@gmail.com>
2025-05-26 22:48:14 +0900

    add camera

---
+++ src/robot/main.py
@@ -0,0 +1,36 @@
+import cv2
+from flask import Flask, render_template, Response
+
+from camera import Camera
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return "Hello World!"
+
+@app.route("/stream")
+def stream():
+	return render_template("stream.html")
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+
+		if frame is not None:
+			yield (b"--frame\r\n"
+				b"Content-Type: image/jpeg\r\n\r\n" + frame.tobytes() + b"\r\n")
+		else:
+			print("frame is none")
+
+@app.route("/video_feed")
+def video_feed():
+	return Response(gen(Camera()),
+			mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host="0.0.0.0", port=5000)
+
