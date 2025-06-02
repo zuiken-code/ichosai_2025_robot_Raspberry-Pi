e35006ea4521a20caca36bc90772e2e947ee3543
zuiken-code <zuiken2022robo@gmail.com>
2025-06-02 22:10:06 +0900

    add motor

--- src/robot/components/motor.py
+++ src/robot/components/motor.py
@@ -2,12 +2,25 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from mode import ControllMode
+from smbus import SMBus
+
+arduino = 0x8
+i2cbus = SMBus(1)
+
+motorOn = 0x0
+motorOff = 0x1
+blueOn = 0x2
+blueOff = 0x3
 
 def moveFront():
     print("front")
+    i2cbus.write_byte(arduino, motorOn)
+
 
 def stop():
     print("stop")
+    i2cbus.write_byte(arduino, motorOff)
+
 
 def applyMode(mode):
     if mode == ControllMode.MoveFront:
