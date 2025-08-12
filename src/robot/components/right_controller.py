989d98d03494384f51911920a60e072cdaed5fb9
zuiken-code <zuiken2022robo@gmail.com>
2025-08-12 16:09:37 +0900

    add jyoistick

--- src/robot/components/right_controller.py
+++ src/robot/components/right_controller.py
@@ -1,9 +1,22 @@
+import time
 import pygame
 import os
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
 pygame.joystick.init()
+
+X_button = 2
+B_button = 0
+
+stick_x = 1
+deadband = 0.1
+
+def dead_band(number, deadband):
+    if abs(number) < deadband:
+        return 0
+    else:
+        return number
 
 def get_joycon_data():
     pygame.joystick.quit()
@@ -12,7 +25,7 @@ def get_joycon_data():
     joystick_count = pygame.joystick.get_count()
     if joystick_count == 0:
         print("接続出来てません")
-        return {"status": "disconnected", "button": {}}
+        return {"status": "disconnected"}
 
     # 1つ目のジョイスティックを取得（Joy-Conが1台だけ接続されている前提）
     joystick = pygame.joystick.Joystick(0)
@@ -21,7 +34,17 @@ def get_joycon_data():
     # イベントポンプを通さないとボタン状態が更新されない
     pygame.event.pump()
 
-    # 例えば、ボタン0が "X" かどうかは Joy-Con によって異なる → 調べて使う
-    x_button = joystick.get_button(2)  # 適宜ボタン番号を変えて
+    x_push = joystick.get_button(X_button)
+    b_push = joystick.get_button(B_button)
 
-    return {"status": "connected", "button": x_button}
+    stick_value = dead_band(joystick.get_axis(stick_x),deadband)
+
+    return {"status": "connected", "x_push": x_push, "b_push": b_push, "stick_value": stick_value}
+
+def main():
+    while True:
+        print(get_joycon_data())
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
