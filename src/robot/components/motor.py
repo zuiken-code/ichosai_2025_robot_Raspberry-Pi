5937d857f6db8db9293fd55fbaf4e772ab436a04
zuiken-code <zuiken2022robo@gmail.com>
2025-08-13 16:55:29 +0900

    add set motor power

--- src/robot/components/motor.py
+++ src/robot/components/motor.py
@@ -7,26 +7,48 @@ from smbus import SMBus
 arduino = 0x8
 i2cbus = SMBus(1)
 
+# 前に進むときの両輪に流すパワー (0〜255)
+front_power = 20
+
+right_power = 0
+left_power = 0
+
+# ジョイスティックの値をパワーの増減に変換する倍率
+# 完全に倒した (1 or -1)の時に倒した側の車輪のパワーが0になるように
+# よってfront_powerと同じ値になった (操作感によって変更する可能性あり)
+magnification = front_power
+
+def set_power(stick_value,magnification):
+    right = front_power + stick_value * magnification
+    left = front_power - stick_value * magnification
+    return right, left
+
 def moveFront():
     print("front")
+    print(front_power,front_power)
 
 def stop():
     print("stop")
+    print(0,0)
 
-def moveRight():
+def moveRight(stick_value):
+    right_power, left_power = set_power(stick_value,magnification)
     print("right")
+    print(left_power, right_power)
 
-def moveLeft():
+def moveLeft(stick_value):
+    right_power, left_power = set_power(stick_value,magnification)
     print("left")
+    print(left_power, right_power)
 
-def applyMode(mode):
+def applyMode(mode,stick_value):
     if mode == ControllMode.Stop:
         stop()
     elif mode == ControllMode.MoveFront:
         moveFront()
     elif mode == ControllMode.MoveRight:
-        moveRight()
+        moveRight(stick_value)
     elif mode == ControllMode.MoveLeft:
-        moveLeft()
+        moveLeft(stick_value)
     else:
         stop()
